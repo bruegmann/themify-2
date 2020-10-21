bf624ad8689a7f3cf04254500cee1dd15b2ad964
wag-bsw <wag@patorg.de>
2020-10-21 13:01:44 +0200

    #16 BugFix Template is sometimes null

--- src/components/ConfigHome.tsx
+++ src/components/ConfigHome.tsx
@@ -24,9 +24,12 @@ export default function ConfigHome(props: any) {
     useEffect(() => {
         var ls = String(localStorage.getItem("template"));
 
-        console.log(props.user)
-        if (ls !== null && props.user !== undefined) {
+        console.log(ls)
+        if (ls !== "null" && props.user !== undefined) {
             setSelected(ls);
+        }
+        else{
+            setSelected("none");
         }
 
     }, [props])
